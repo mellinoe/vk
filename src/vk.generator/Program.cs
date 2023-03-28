6311740a859504b161f8cafb440c6e023b86be33
Marvel Renju <marvelrenju1@gmail.com>
2023-03-28 21:48:52 +0100

    Vk.generator: Target net6.0

--- src/vk.generator/Program.cs
+++ src/vk.generator/Program.cs
@@ -9,13 +9,25 @@ namespace Vk.Generator
     {
         public static int Main(string[] args)
         {
-            string outputPath = AppContext.BaseDirectory;
 
-            ArgumentSyntax.Parse(args, s =>
-            {
-                s.DefineOption("o|out", ref outputPath, "The folder into which code is generated. Defaults to the application directory.");
-            });
+            var outFolderOption = new Option<String>(
+                name: "--out",
+                description: "The folder into which code is generated. Defaults to the application directory.",
+                getDefaultValue: () => AppContext.BaseDirectory);
 
+            var rootCommand = new RootCommand("Vk Bindings generator");
+
+            rootCommand.SetHandler((DirPath) => {
+                Generate(DirPath);
+            }, outFolderOption);
+
+            rootCommand.AddOption(outFolderOption);
+
+            return rootCommand.Invoke(args);
+        }
+
+        public static int Generate(string outputPath) 
+        {
             Configuration.CodeOutputPath = outputPath;
 
             if (File.Exists(outputPath))
